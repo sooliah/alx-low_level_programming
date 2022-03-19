f3807211eb3d2d8fcc4a14c013f3a86412f20824
sooliah <monshalewa@gmail.com>
2022-03-19 09:12:37 -0700

    It is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,21 +4,9 @@
  * _isalpha - checks for alphabetic character
  * @c: c is an ascii character
  *
- * Return: 1 (if letter)
+ * Return: 1 if c is a letter, 0 otherwise
  */
-
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
