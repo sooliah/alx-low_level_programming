095354a951a33287c72466977fa077ab5e90f31a
sooliah <monshalewa@gmail.com>
2022-03-24 16:43:40 -0700

    Task ten

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,7 +13,7 @@ int main(void)
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are no allowed to modify p
-	 * - only one staement
+	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
 	p[5] = 98;
